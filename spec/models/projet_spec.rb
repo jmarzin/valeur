4f09228e1d0acf6eb24c5c71ec3fd2145ec06b8e
jmarzin <jacques.marzin@free.fr>
2013-04-20 20:33:01 +0200

    Samedi 20 heures

--- spec/models/projet_spec.rb
+++ spec/models/projet_spec.rb
@@ -1,5 +1,13 @@
+# encoding: utf-8
 require 'spec_helper'
 
-#describe Projet do
-#  pending "add some examples to (or delete) #{__FILE__}"
-#end
+describe Projet do
+  it "un nouveau projet a le statut a_l_etude" do
+    FactoryGirl.build(:projet).a_l_etude?.should be_true
+  end
+  it "un projet à l'étude qu'on abandonne devient abandonné" do
+    projet = FactoryGirl.build(:projet) # Projet.new
+    projet.abandonner!.should be_true
+    projet.abandonne?.should be_true
+  end
+end
